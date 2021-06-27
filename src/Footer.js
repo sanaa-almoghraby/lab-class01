46298b5051d5a1d6f3981e36555a7646cf6437ee
sanaa-almoghraby <sanaa.almoghraby@gmail.com>
2021-06-27 21:16:37 +0300

    lab1

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Footer extends React.Component {
+    render() {
+      return (
+        <div>
+             <p> © (ASAC) Sanaa AL_Moghraby </p>
+        </div>
+      )
+    
+  }
+}
+export default Footer;
+
