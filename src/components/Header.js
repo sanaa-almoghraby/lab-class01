3829af38bedb6751d0b9ef5cfbcd6c1529f51bfe
sanaa-almoghraby <sanaa.almoghraby@gmail.com>
2021-06-30 19:04:02 +0300

    lab04

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,9 @@ import React from 'react'
 class Header extends React.Component {
     render() {
       return (
-        <div>
-          <h1> HornedBeasts </h1>
+        <div className="header">
+          
+          <h1> Horned Beasts </h1>
   
         </div>
       )
